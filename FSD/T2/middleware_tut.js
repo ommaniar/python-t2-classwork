7c38881ab1a24ac79e9b380dd3c17b1958252344
ommaniar <109684810+ommaniar@users.noreply.github.com>
2024-06-05 10:31:54 +0530

    Update middleware_tut.js

--- FSD/T2/middleware_tut.js
+++ FSD/T2/middleware_tut.js
@@ -20,6 +20,19 @@ app.get('/ee',(req,res)=>{
     res.send();
 })
 
+app.use("/",(req,res,next)=>{
+    console.log("initialized");
+    res.set("content-type","text/html");
+    res.write("<strong>First</strong>");
+    next();
+},(req,res,next)=>{
+    res.write("<p>Adding of 5 + 5 " + (5+5) + "</p>");
+    next();
+},(req,res)=>{
+    res.write("<h1 style='color:tomato'>Hello World</h1>");
+    res.send();
+})
+
 app.listen(3000);
 
 
