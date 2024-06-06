30f42877efacab70eb154076705c3fb0550c4142
ommaniar <109684810+ommaniar@users.noreply.github.com>
2024-06-06 09:59:24 +0530

    ch-4 in python updated

--- FSD/T2/question_8/app.js
+++ FSD/T2/question_8/app.js
@@ -1,40 +1,3 @@
-express = require("express");
-app = express();
-
-const cb = (req,res,next)=>{
-    console.log("initialized");
-    res.set("content-type","text/html");
-    res.write("<strong>First</strong>");
-    next();
-}
-
-const cb1 = (req,res,next)=>{
-    res.write("<p>Adding of 5 + 5 " + (5+5) + "</p>");
-    next();
-}
-
-app.use('/ee',cb,cb1);
-
-app.get('/ee',(req,res)=>{
-    res.write("<h1>Hello World</h1>");
-    res.send();
-})
-
-app.use("/",(req,res,next)=>{
-    console.log("initialized");
-    res.set("content-type","text/html");
-    res.write("<strong>First</strong>");
-    next();
-},(req,res,next)=>{
-    res.write("<p>Adding of 5 + 5 " + (5+5) + "</p>");
-    next();
-},(req,res)=>{
-    res.write("<h1 style='color:tomato'>Hello World</h1>");
-    res.send();
-})
-
-app.listen(3000);
-
 // Write express js script to perform following tasks.
 // 1. Create one html file which contains one text field for name, email field and checkbox for subscription. 
 // Html file will be loaded on home page. Email and name fields are required fields.
@@ -48,3 +11,12 @@ app.listen(3000);
 // subscription” message will be printed and “logout” link will be displayed under the message. If 
 // user clicks logout link then he/she will be redirected to the home page.
 // Use concept of the middleware and you can use any of http methods(get/post).
+
+const exp = require('express');
+const app = exp();
+
+app.use(__dirname,{index:"form.html"})
+
+app.get("/login",(req,res,next)=>{
+    
+});
