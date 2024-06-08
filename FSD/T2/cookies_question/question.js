42ac9ad1672ebda460c24b2f467b3960e8d314a4
ommaniar <109684810+ommaniar@users.noreply.github.com>
2024-06-08 13:30:43 +0530

    Python folder  updated

---
+++ FSD/T2/cookies_question/question.js
@@ -0,0 +1,9 @@
+// you have been assigned to develop a user feedback form for the website using express, js and cookies. Implement the following requirements:
+/**
+ * 1. create a form with the following feed. 
+ *  a. name (email) b. email (email) c. message (textarea) d. rating (radio) trash, bad, average, good, excellent
+ * 2. when the user submit the form, store thier feedback information. Name, email, message, rating in a cookie named feedback that expires in 10 secodns
+ * 3. display a confirmation message to the user after successfully submitting the form and create a link that displays the feedback details stored in the feedback cookie.
+ * 4. when the user click to the link retrieve the info from cookkie and dsiplay in the page that also includes the link feedback details page to logout.
+ * 5. when the user clicks the logout link it should redirect to home page.
+ */
